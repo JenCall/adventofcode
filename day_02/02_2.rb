afd28dfa28e4b829d8aad9766b452ba6029c68c8
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-26 20:50:34 +0100

    add solution part 2

--- day_02/02_2.rb
+++ day_02/02_2.rb
@@ -6,23 +6,23 @@ file = File.open("input.txt")
 input = file.readlines.map(&:chomp)
 
 #PART 1
-# # define valid passwords variable
-# valid_password = []
+# define valid passwords variable
+valid_password = []
 
-# # loop through the list
-# input.each do |string|
-#     # define variables
-#     num_1 = string.split(/-/).first.to_i
-#     num_2 = string.split(/-/, 2)[1][0..1].to_i
-#     letter = string.split(/:/).first[-1]
-#     password = string[7..string.length]
-#     count = password.count(letter)
+# loop through the list
+input.each do |string|
+    # define variables
+    num_1 = string.split(/-/).first.to_i
+    num_2 = string.split(/-/, 2)[1][0..1].to_i
+    letter = string.split(/:/).first[-1]
+    password = string[7..string.length]
+    count = password.count(letter)
 
-#     # conditional statement
-#     valid_password << password if (count >= num_1 && count <= num_2)
-# end
-# # print count of valid passwords
-# p valid_password.count
+    # conditional statement
+    valid_password << password if (count >= num_1 && count <= num_2)
+end
+# print count of valid passwords
+p valid_password.count
 
 #PART 2
 # define valid passwords variable
